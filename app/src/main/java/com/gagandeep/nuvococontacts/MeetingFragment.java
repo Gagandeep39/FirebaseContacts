e443504376bf8212cb77b932ecb50e6ba774e16e
Gagandeep39 <singh.gagandeep3911@gmail.com>
2019-02-15 23:28:15 +0530

    Removed Unnecessary files

--- app/src/main/java/com/gagandeep/nuvococontacts/MeetingFragment.java
+++ app/src/main/java/com/gagandeep/nuvococontacts/MeetingFragment.java
@@ -9,18 +9,14 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 public class MeetingFragment extends Fragment {
     DatabaseReference databaseReferenceUser;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_search, container, false);
-        // FirebaseApp.initializeApp(getContext());
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        databaseReferenceUser = database.getReference();
-        findViews(v);
+        View v = inflater.inflate(R.layout.fragment_meeting, container, false);
+
         return v;
     }
 
